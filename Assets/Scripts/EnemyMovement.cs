c3bd213bf8fe3497abd7a17bcce70af71eb1b84c
Gustas <gustass1@gmail.com>
2020-04-20 02:32:03 +0200

    feat: added enemy and enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Fire").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
