f6ac5c933d79cc91864c06ecc4b7200e5222ec71
Gustas <gustass1@gmail.com>
2020-04-20 21:57:59 +0200

    feat: created enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,18 +4,56 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private Transform target;
     public float speed = 2f;
+    public float sensorRadius = 1f;
+
+    private float currentSpeed;
+    private int previousColliderLength = 0;
+    private bool isMoving = true;
+    private Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Fire").transform;
+        currentSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        // if(Vector2.Distance(transform.position, target.position) > 0.75f){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, currentSpeed * Time.deltaTime);
+            Collider2D[] col =  Physics2D.OverlapCircleAll(transform.position, sensorRadius);
+            if(col.Length > 0 && (col.Length != previousColliderLength)){
+                isMoving = !isMoving;
+                if(isMoving){
+                    currentSpeed = speed;
+                }else{
+                    currentSpeed = 0;
+                }
+            }
+                previousColliderLength = col.Length;
     }
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, sensorRadius); 
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            Destroy(this.gameObject);
+        }
+    }
+
+    // void OnTriggerExit2D(Collider2D other){
+    //             Debug.Log("Exited");
+
+    //     if(other.tag == "LightCone"){
+    //                 Debug.Log("Exited right one");
+
+    //         currentSpeed = speed;
+    //     }
+    // }
 }
